727c0ea8195daf4a3a1414e1f6365ca539b12d22
Ezzanz Youness <youness.ezzanz@sahadina.com>
2023-04-09 18:50:55 +0000

    Jokes App method 1

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const JokesApp(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -28,35 +29,109 @@ class JokesApp extends StatefulWidget {
 }
 
 class _JokesAppState extends State<JokesApp> {
+  List jokes = [
+    {
+      "joke":
+          "“Once, my father came home and found me in front of a roaring fire. That made my father very mad, as we didn’t have a fireplace.”",
+      "author": '—Victor Borge',
+    },
+    {
+      "joke":
+          "In Denver, the members of a Sunday­-school class were asked to set down their favorite biblical truths. One youngster laboriously printed: “Do one to others as others do one to you.”",
+      "author": "—Lee Olson, The Denver Post",
+    },
+    {
+      "joke":
+          "Why did the soccer player take so long to eat dinner? Because he thought he couldn’t use his hands.",
+      "author": "—Jhon Olson",
+    },
+    {
+      "joke":
+          "What did the policeman say to his hungry stomach? “Freeze. You’re under a vest.”",
+      "author": "—Albert Capsic",
+    },
+  ];
+
+  var myIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text(
-              "Once, my father came home and found me in front of a roaring fire. That made my father very mad, as we didn’t have a fireplace",
-              style: TextStyle(
-                fontFamily: "HappyMonkey",
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Jokes",
+            style: TextStyle(
+              fontFamily: "HappyMonkey",
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
-            SizedBox(height: 25),
-            Text(
-              "Victor Borge",
-              style: TextStyle(
-                fontFamily: "HappyMonkey",
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.orange,
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                jokes[myIndex]['joke'],
+                style: const TextStyle(
+                  fontFamily: "HappyMonkey",
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
-            ),
-          ],
+              const SizedBox(height: 28),
+              Text(
+                jokes[myIndex]['author'],
+                style: const TextStyle(
+                  fontFamily: "HappyMonkey",
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 28),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                        if (myIndex > 0) {
+                          myIndex--;
+                        }
+                      });
+                    },
+                    backgroundColor: Colors.orange,
+                    child: const Icon(
+                      Icons.arrow_left,
+                      size: 48,
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  FloatingActionButton(
+                    onPressed: () {
+                      setState(() {
+                        if (myIndex < jokes.length - 1) {
+                          myIndex++;
+                        }
+                      });
+                    },
+                    backgroundColor: Colors.orange,
+                    child: const Icon(
+                      Icons.arrow_right,
+                      size: 48,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
